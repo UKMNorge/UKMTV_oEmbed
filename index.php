268cb0d74e0fcc6a2dbab6a1c79e0bbbd9806031
Marius Mandal <mariusmandal@gmail.com>
2013-10-14 11:12:23 +0200

    working (?) oembed

--- index.php
+++ index.php
@@ -9,29 +9,41 @@ $url = urldecode($_GET['url']);
 $start  = strrpos($url, '/');
 $ID = substr($url, $start+1);
 
-var_dump($url);
-var_dump($ID);
-
 $UKMTV = new tv($ID);
 
-var_dump($UKMTV);
+if(!$UKMTV->id)
+	die(json_encode(false));
+	
+$UKMTV->size();
 
-$width = isset($_GET['maxwidth']) ? $_GET['maxwidth'] : false;
-$height = isset($_GET['maxheight']) ? $_GET['maxheight'] : false;
+$maxwidth = isset($_GET['maxwidth']) ? $_GET['maxwidth'] : false;
+$maxheight = isset($_GET['maxheight']) ? $_GET['maxheight'] : false;
+
+if($maxwidth && $UKMTV->width > $maxwidth) {
+	$width = $maxwidth;
+	$height = $maxwidth / $UKMTV->ratio;
+} else {
+	$width = $UKMTV->width;
+	$height = $UKMTV->height;
+}
+
+if($maxheight && $UKMTV->height > $maxheight) {
+	$height = $maxheight;
+	$width = $UKMTV->ratio * $height;
+}
 
 $oembed = array('type' => 'video',
 				'version' => '1.0',
 				'title' => $UKMTV->title,
 				'author_name' => 'UKM Norge',
-				'author_url' => '://ukm.no',
+				'author_url' => 'http://ukm.no',
 				'provider_name' => 'UKM-TV',
-				'provider_url' => '://tv.ukm.no',
-				'thumbnail_url' => $UKMTV->image,
-				'thumbnail_width' => $UKMTV->image,
-				'thumbnail_heigh' => $UKMTV->image,
-				'html' => $UKMTV->embed,
-				'width' => $UKMTV->width,
-				'height' => $UKMTV->height
+				'provider_url' => 'http://tv.ukm.no',
+				'thumbnail_url' => $UKMTV->image_url,
+				'thumbnail_width' => $UKMTV->width,
+				'thumbnail_height' => $UKMTV->height,
+				'html' => $UKMTV->embedcode($width),
+				'width' => $width,
+				'height' => $height
 				);
-				
 die(json_encode($oembed));
