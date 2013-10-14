ac9911533bb7712131ca2747526468e65a8805f4
Marius Mandal <mariusmandal@gmail.com>
2013-10-14 11:01:38 +0200

    debug infos

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-<h2>oEmbed to come</h2>
+<?php
+if(!isset($_GET['url'])) {
+	die(json_encode(false));
+}
+
+$url = urldecode($_GET['url']);
+
+$start = strlen('http://tv.ukm.no/');
+$stop  = strpos($url, '/', $start);
+$ID = substr($url, $start, $stop-$start);
+
+$UKMTV = new tv($ID);
+
+var_dump($UKMTV);
+
+$width = isset($_GET['maxwidth']) ? $_GET['maxwidth'] : false;
+$height = isset($_GET['maxheight']) ? $_GET['maxheight'] : false;
+
+$oembed = array('type' => 'video',
+				'version' => '1.0',
+				'title' => $UKMTV->title
+				'author_name' => 'UKM Norge',
+				'author_url' => '://ukm.no',
+				'provider_name' => 'UKM-TV',
+				'provider_url' => '://tv.ukm.no',
+				'thumbnail_url' => $UKMTV->image,
+				'thumbnail_width' => $UKMTV->image,
+				'thumbnail_heigh' => $UKMTV->image,
+				'html' => $UKMTV->embed,
+				'width' => $UKMTV->width,
+				'height' => $UKMTV->height
+				);
+				
+die(json_encode($oembed));
